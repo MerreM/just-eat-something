265376df29d3951bcb90be6a5fae305b0d672a0a
Mike Readhead <masked.jokertdc@gmail.com>
2014-03-12 21:30:43 +0000

    Cheers.

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 import random
+import urllib
+from pprint import pprint
+
+POST_CODE_QUERY = "http://www.just-eat.co.uk/Postcode/Cuisines?"
+
+GET_RESTARUNTS = "http://www.just-eat.co.uk/area/nr1-norwich/"
+
+
+
+
 
 def filter_styles(styles):
     ## Filter based on choices
@@ -10,7 +20,13 @@ def filter_styles(styles):
 def get_available_styles(location):
     ## Get all options
     ## Remove all those that all closed
-    styles = None
+    response = requests.get(POST_CODE_QUERY+urllib.urlencode(location))
+    styles = dict(response.json())
+    styles = styles["cuisines"]
+    styles_dict = {}
+    for style in styles:
+        styles_dict[style["Name"]]=style["FriendlyName"]
+    pprint(styles_dict)
     return filter_styles(styles)
 
 def get_styles_for_location(location):
@@ -33,18 +49,22 @@ def get_catergories(menu):
     ## Get Starters/Beverages/Desserts
     ## Errything else is Mains.
     ## Each main must have Spice/Veg/Vegan Attrs.
+    pass
 
 def get_menu(restaurant):
     menu = None
     catergories = get_catergories(menu)
-
-
+    return menu
 
 def main():
-    location = raw_input("Post code")
+    postcode = raw_input("Post code>>>\n")
+    location = {
+        "postcode":postcode,
+    }
     styles = get_styles_for_location(location)
     style = random.choice(styles)
     restaurant = get_restaurant(style)
+    menu = get_menu(restaurant)
 
-
-
+if __name__ == "__main__":
+    main()
